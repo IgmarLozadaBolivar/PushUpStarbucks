fc3a3f7b8984d892f8a6df8935884b9acf00c924
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-12-13 18:39:39 -0500

    feat: :lipstick: HTML, CSS and JS files for the completion of web development

---
+++ html/js/script.js
@@ -0,0 +1,6 @@
+Swal.fire({
+    title: '¡Bienvenido a Starbucks',
+    text: 'Explora nuestro variado menu',
+    iconHtml: '<img src="https://img.icons8.com/fluency/48/starbucks.png" class="icono-personalizado">',
+    confirmButtonText: 'Aceptar',
+});
